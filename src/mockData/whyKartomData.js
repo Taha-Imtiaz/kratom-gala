39165f59c8ad95acd75f9ebc95d31596ef872f63
tahaimtiaz1996@gmail.com <tahaimtiaz1996@gmail.com>
2023-08-24 02:45:18 +0500

    resolve some changes

--- src/mockData/whyKartomData.js
+++ src/mockData/whyKartomData.js
@@ -1,31 +1,31 @@
-import Delivery from "../assets/images/Delivery.png";
-import FastDelivery from "../assets/images/fast-delivery (1).png"
-import Support from "../assets/images/support.png"
-import Guarantee from "../assets/images/Guarantee.png"
+import Delivery from "../assets/images/delivery.png";
+import FastDelivery from "../assets/images/fast-delivery (1).png";
+import Support from "../assets/images/support.png";
+import Guarantee from "../assets/images/guarantee.png";
 
 export const kartomData = [
   {
     image: Delivery,
     id: 1,
-    text:"Free Delivery",
-    subText: "Orders over $100 delivered free via USPS Priority Mail"
+    text: "Free Delivery",
+    subText: "Orders over $100 delivered free via USPS Priority Mail",
   },
   {
     image: FastDelivery,
     id: 2,
-    text:"Fast Delivery",
-    subText:"Orders over $100 delivered free via USPS Priority Mail"
+    text: "Fast Delivery",
+    subText: "Orders over $100 delivered free via USPS Priority Mail",
   },
   {
     image: Support,
     id: 3,
-    text:"The Help",
-    subText:"Phone & Email support. Your concierge service."
+    text: "The Help",
+    subText: "Phone & Email support. Your concierge service.",
   },
   {
     image: Guarantee,
     id: 4,
-    text:"More Then 9800 Satisfied Reviews",
-    subText:"Phone & Email support. Your concierge service."
+    text: "More Then 9800 Satisfied Reviews",
+    subText: "Phone & Email support. Your concierge service.",
   },
 ];
